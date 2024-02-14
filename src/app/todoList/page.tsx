3993f7f715acc4626d38c02b77c21566d8175a40
sailesh <vermasailesh271@gmail.com>
2024-02-14 17:26:49 +0530

    added todo home page

---
+++ src/app/todoList/page.tsx
@@ -0,0 +1,9 @@
+export default function ToDoPage() {
+  return (
+    <>
+      <div className="container mx-auto min-h-screen">
+        <h1>This Is TODO Page wil show the here</h1>
+      </div>
+    </>
+  );
+}
